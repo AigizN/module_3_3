74d1bf378f826341bfc65f3a0793a833abdccef0
AigizN <aigiznag@gmail.com>
2024-12-25 14:35:16 +0700

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,29 @@
+
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+
+
+print_params()
+print('--------------------------------')
+
+print_params(2, 'столбец', False)
+print_params(3, 'None')
+print_params(a='один', b='два', c='три')
+print_params(a='один', b='два')
+print_params(b='два', c='три')
+print_params(a='один', c='три')
+print_params(a='один')
+print_params(b='два')
+print_params(c='три')
+print_params()
+print('--------------------')
+
+print_params(b=25)
+print_params(c=[1, 2, 3])
+print('--------------------')
+
+values_list = [9.8, 'const', True]
+values_dict = {'a': 'else', 'b': False, 'c': 0}
+print_params(*values_list)
+print_params(**values_dict)
+print('----------------------')
